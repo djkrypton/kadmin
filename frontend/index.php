2d377a0d1be437ec2440365bd27296322ba68061
KryptonJD <alain.cuisenier@gmail.com>
2023-11-04 09:51:14 +0100

    Change variable/autoload name

--- frontend/index.php
+++ frontend/index.php
@@ -8,5 +8,5 @@ require BACKEND_DIR . '/vendor/autoload.php';
 require BACKEND_DIR . '/middleware/autoload.php';
 require BACKEND_DIR . '/controller/autoload.php';
 require BACKEND_DIR . '/model/autoload.php';
-require BACKEND_DIR . '/variable/variable.php';
+require BACKEND_DIR . '/variable/autoload.php';
 require BACKEND_DIR . '/router/router.php';
