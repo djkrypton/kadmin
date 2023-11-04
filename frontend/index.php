20419a8dbdad673ae66125f592424c39f35ffd4c
KryptonJD <alain.cuisenier@gmail.com>
2023-11-04 09:00:19 +0100

    Change load file name by autoload.php

--- frontend/index.php
+++ frontend/index.php
@@ -5,8 +5,8 @@ session_start();
 require '../backend/config/config.php';
 
 require BACKEND_DIR . '/vendor/autoload.php';
-require BACKEND_DIR . '/middleware/middleware.php';
-require BACKEND_DIR . '/controller/controller.php';
-require BACKEND_DIR . '/model/model.php';
+require BACKEND_DIR . '/middleware/autoload.php';
+require BACKEND_DIR . '/controller/autoload.php';
+require BACKEND_DIR . '/model/autoload.php';
 require BACKEND_DIR . '/variable/variable.php';
 require BACKEND_DIR . '/router/router.php';
